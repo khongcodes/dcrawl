86055709adb139a773ba4650bbe41f61ceb6b6d5
KHong <albtrossfacefall@gmail.com>
2024-09-13 06:27:13 -0400

    complete main menu exit action system

--- src/plugins/manage_state_plugin/main_menu_plugin.rs
+++ src/plugins/manage_state_plugin/main_menu_plugin.rs
@@ -7,138 +7,169 @@ impl Plugin for MainMenuPlugin {
    fn build(&self, app: &mut App) {
       app.add_systems(OnEnter(GameModeState::Menu), setup_mainmenu);
       app.add_systems(OnExit(GameModeState::Menu), cleanup_mainmenu);
-      app.add_systems(Update, run_mainmenu.run_if(in_state(GameModeState::Menu)));
+      app.add_systems(
+         Update, 
+         (button_style_system, main_menu_action_system).run_if(in_state(GameModeState::Menu))
+      );
    }
 }
 
 const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
 const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+const HOVERED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
 
-#[derive(Resource)]
+#[derive(Resource, std::fmt::Debug)]
 struct MainMenuData {
-   continue_button_entity: Entity,
-   exit_button_entity: Entity
+   screen_node: Entity
 }
 
 fn setup_mainmenu(mut commands: Commands) {
-   let continue_button_entity = commands
-      .spawn(NodeBundle {
-            style: Style{
-               width: Val::Percent(100.0),
-               height: Val::Percent(50.0),
-               align_items: AlignItems::Center,
-               justify_content: JustifyContent::Center,
-               ..default()
-            },
-            ..default()
-      })
-      .with_children(|parent| { parent.spawn(
-            ButtonBundle {
-               style: Style {
-                  width: Val::Px(150.0),
-                  height: Val::Px(65.0),
-                  border: UiRect::all(Val::Px(5.0)),
 
-                  // horizontally center child text
-                  justify_content: JustifyContent::Center,
-                  // vertically center child text
-                  align_content: AlignContent::Center,
-                  ..default()
-               },
-               border_color: BorderColor(Color::BLACK),
-               border_radius: BorderRadius::MAX,
-               background_color: NORMAL_BUTTON.into(),
-               ..default()
-            })
-            .with_children(|parent| {
-               parent.spawn(TextBundle::from_section(
-                  "Continue", 
-                  TextStyle { 
-                        font_size: 40.0, 
-                        color: Color::srgb(0.9, 0.9, 0.9), 
-                        ..default() 
-                  }
-               ));
-            });
-      })
-      .id();
+   // render a screen
+   let screen_node = commands.spawn(NodeBundle {
+      style: Style {
+         width: Val::Percent(100.0),
+         height: Val::Percent(100.0),
+         align_items: AlignItems::Center,
+         justify_content: JustifyContent::Center,
+         ..default()
+      },
+      ..default()
+   }).id();
 
-   let exit_button_entity = commands
-      .spawn(NodeBundle {
-         style: Style{
-            top: Val::Percent(50.0),
-            width: Val::Percent(100.0),
-            height: Val::Percent(50.0),
-            align_items: AlignItems::Center,
+   let continue_button_entity = commands.spawn(( 
+      ButtonBundle {
+         style: Style {
+            width: Val::Px(150.0),
+            height: Val::Px(65.0),
+            border: UiRect::all(Val::Px(5.0)),
             justify_content: JustifyContent::Center,
+            align_content: AlignContent::Center,
             ..default()
          },
+         border_color: BorderColor(Color::BLACK),
+         border_radius: BorderRadius::MAX,
+         background_color: NORMAL_BUTTON.into(),
          ..default()
-      })
-      .with_children(|parent| { parent.spawn(
-         ButtonBundle {
-            style: Style {
-               width: Val::Px(150.0),
-               height: Val::Px(65.0),
-               border: UiRect::all(Val::Px(5.0)),
-               justify_content: JustifyContent::Center,
-               align_content: AlignContent::Center,
-               ..default()
-            },
-            border_color: BorderColor(Color::BLACK),
-            border_radius: BorderRadius::MAX,
-            background_color: NORMAL_BUTTON.into(),
-            ..default()
-         })
-         .with_children( |parent| {
-            parent.spawn(TextBundle::from_section(
-               String::from("Exit"),
-               TextStyle {
-                  font_size: 80.0,
-                  color: Color::srgb(0.9, 0.9, 0.9), 
-                  ..default() 
-               }
-            ));
-         });
-      })
-      .id();
+      },
+      MainMenuButtonAction::Continue
+   ))
+   .with_children(|parent| { 
+      parent.spawn( TextBundle::from_section(
+         "Continue",
+         TextStyle { 
+            font_size: 40.0, 
+            color: Color::srgb(0.9, 0.9, 0.9), 
+            ..default() 
+         }
+      ));
+   }).id();
 
-   commands.insert_resource(MainMenuData { exit_button_entity, continue_button_entity });
+   let quit_button_entity = commands.spawn((
+      ButtonBundle {
+         style: Style {
+            width: Val::Px(150.0),
+            height: Val::Px(65.0),
+            border: UiRect::all(Val::Px(5.0)),
+            justify_content: JustifyContent::Center,
+            align_content: AlignContent::Center,
+            ..default()
+         },
+         border_color: BorderColor(Color::BLACK),
+         border_radius: BorderRadius::MAX,
+         background_color: NORMAL_BUTTON.into(),
+         ..default()
+      },
+      MainMenuButtonAction::Quit
+   ))
+      .with_children(|parent| { 
+         parent.spawn(TextBundle::from_section(
+            "Exit", 
+            TextStyle {
+               font_size: 80.0,
+               color: Color::srgb(0.9, 0.9, 0.9), 
+               ..default() 
+            }
+         ));
+      }
+   ).id();
+
+   commands.entity(screen_node).push_children(&vec![continue_button_entity, quit_button_entity]);
+   commands.insert_resource(MainMenuData { screen_node });
 }
 
 
-// TODO: when pressed, use commands.run_system_with_input() to change game state
-fn run_mainmenu(
+fn cleanup_mainmenu(
+   mut commands: Commands,
+   mainmenu_data: Res<MainMenuData>
+) {
+   commands.entity(mainmenu_data.screen_node).despawn_recursive();
+}
+
+
+/////////////////////////////////
+// MENU ACTIONS
+#[derive(Component)]
+enum MainMenuButtonAction {
+   Continue,
+   Quit
+}
+
+
+
+
+fn button_style_system(
    mut interaction_query: Query<
-      (
-            &Interaction,
-            &mut BackgroundColor,
-            &mut BorderColor,
-            &Children,
-      ),
+      ( &Interaction, &mut BackgroundColor, &mut BorderColor ),
       (Changed<Interaction>, With<Button>)
    >,
-   mut text_query: Query<&mut Text>
 ) {
-   for (interaction, mut color, mut border_color, children) in &mut interaction_query {
-      let mut text = text_query.get_mut(children[0]).unwrap();
+   for (
+      interaction,
+      mut background_color,
+      mut border_color,
+   ) in &mut interaction_query {
+
       match *interaction {
-            Interaction::Pressed => {
-               text.sections[0].value = "CONTINUING".to_string();
-               *color = PRESSED_BUTTON.into();
-               border_color.0 = bevy::color::palettes::basic::RED.into();
+         Interaction::Pressed => {
+            *background_color = PRESSED_BUTTON.into();
+            border_color.0 = bevy::color::palettes::basic::RED.into();
+         }
+         Interaction::Hovered => {
+            *background_color = HOVERED_BUTTON.into();
+            border_color.0 = bevy::color::palettes::basic::RED.into();
+         }
+         Interaction::None => {
+            *background_color = NORMAL_BUTTON.into();
+            border_color.0 = Color::BLACK;
+         }
+      }
+
+   }
+}
+
+fn main_menu_action_system(
+   interaction_query: Query<
+      (&Interaction, &MainMenuButtonAction),
+      (Changed<Interaction>, With<Button>)
+   >,
+   mut app_exit_events: EventWriter<AppExit>
+) {
+   for (interaction, menu_button_action) in &interaction_query {
+      if interaction == &Interaction::Pressed {
+         match menu_button_action {
+            
+            MainMenuButtonAction::Continue => {
+               println!("continue game (TBD)");
             }
-            Interaction::Hovered => {}
-            Interaction::None => {
-               text.sections[0].value = String::from("Continue");
-               *color = NORMAL_BUTTON.into();
-               border_color.0 = Color::BLACK;
+
+            MainMenuButtonAction::Quit => {
+               app_exit_events.send(AppExit::Success);
             }
+
+         }
       }
    }
 }
 
 
-fn cleanup_mainmenu() {
-
-}
